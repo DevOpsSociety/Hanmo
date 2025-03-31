f535be98dcd743efecce3e314d780f8e0dc99eaa
taenam kim <ktn2674@naver.com>
2025-03-31 15:40:31 +0900

    error 코드 추가

--- src/main/java/org/example/hanmo/error/ErrorExceptionControllerAdvice.java
+++ src/main/java/org/example/hanmo/error/ErrorExceptionControllerAdvice.java
@@ -54,12 +54,10 @@ public class ErrorExceptionControllerAdvice {
     @ExceptionHandler({ForbiddenException.class})
     public ResponseEntity<ErrorEntity> exceptionHandler(final ForbiddenException e) {
         return ResponseEntity.status(e.getErrorCode().getStatus())
-            .body(
-                ErrorEntity.builder()
-                    .errorCode(e.getErrorCode().getCode())
-                    .errorMessage(e.getMessage())
-                    .build());
+                .body(
+                        ErrorEntity.builder()
+                                .errorCode(e.getErrorCode().getCode())
+                                .errorMessage(e.getMessage())
+                                .build());
     }
-
-
 }
