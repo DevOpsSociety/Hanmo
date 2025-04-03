7496c9c82272c1a3c00856d420912562150160b9
taenam kim <ktn2674@naver.com>
2025-04-03 14:43:53 +0900

    로컬환경설정으로 변경

--- src/main/java/org/example/hanmo/HanmoApplication.java
+++ src/main/java/org/example/hanmo/HanmoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(servers = {@Server(url = "https://hanmo.store", description = "Hanmo도메인")})
+//@OpenAPIDefinition(servers = {@Server(url = "https://hanmo.store", description = "Hanmo도메인")})
 @SpringBootApplication
 public class HanmoApplication {
 
