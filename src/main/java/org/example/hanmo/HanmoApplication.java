530307ba49c9dbb2fbdcf9c4f39ba71c06710c93
dahye <dahye9011@naver.com>
2025-04-15 01:00:48 +0900

    chore: OpenAPI 주석 해제

--- src/main/java/org/example/hanmo/HanmoApplication.java
+++ src/main/java/org/example/hanmo/HanmoApplication.java
@@ -1,11 +1,13 @@
 package org.example.hanmo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-// @OpenAPIDefinition(servers = {@Server(url = "https://hanmo.store", description = "Hanmo도메인")})
+@OpenAPIDefinition(servers = {@Server(url = "https://hanmo.store", description = "Hanmo도메인")})
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaAuditing
