830da30910613cebd03068052190272298c316b9
박지훈 <jihun6548@naver.com>
2025-04-20 18:45:33 +0900

    feat: 뒤로가기시 쿼리문 실행되는 오류 수정

--- src/main/java/org/example/hanmo/config/CachesConfig.java
+++ src/main/java/org/example/hanmo/config/CachesConfig.java
@@ -4,13 +4,16 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @Configuration
 public class CachesConfig {
@@ -30,5 +33,15 @@ public class CachesConfig {
             .serializeValuesWith(jsonPair) // JSON 직렬화 적용
         );
     return RedisCacheManager.builder(cf).withInitialCacheConfigurations(configs).build();
+  }
+  @Bean
+  public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+    FilterRegistrationBean<ShallowEtagHeaderFilter> fr = new FilterRegistrationBean<>();
+    fr.setFilter(new ShallowEtagHeaderFilter());
+    fr.addUrlPatterns("/api/*");
+    fr.setName("etagFilter");
+    fr.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+    return fr;
   }
 }
